91909e67b6c3f1c13730eeef54d34afd17805021
Samir55 <discere044@icloud.com>
2017-10-15 00:29:41 +0200

    Fix radix_sort.cpp

--- Sort/radix_sort.cpp
+++ Sort/radix_sort.cpp
@@ -10,7 +10,7 @@
 #include <cmath>
 #include <cstring>
 using namespace std;
-#define lp(i, n)        for(int i=0;i<=(int)(n);++i)
+#define lp(i, n)        for(int i=0;i<(int)(n);++i)
 #define INTEGER_SIZE 32
 
 // ToDo @Samir55 Implement most significant digit radix sort.
@@ -22,17 +22,17 @@ class radix_bit {
 	radix_bit (int b) : bit(b) {
 	}
 	
-	bool operator ()(int value) {
+	bool operator ()(int value) const {
 		// Check the sign bit and return true (means the element sholud be swapped to the right of the vector) if it's negative.
 		if (bit == 31) 
 			return value < 0;
 		else
-			return !(bit & (1 << bit)); // 0 bit to left partition
+			return !(value & (1 << bit)); // 0 bit to left partition
 	}
 };
 
 /// Radix Sort. 
-void radix_sort (vector<int> vec) {
+void radix_sort (vector<int>& vec) {
 	lp (least_sig_bit, INTEGER_SIZE) {
 		stable_partition(vec.begin(), vec.end(), radix_bit(least_sig_bit));
 	}
@@ -45,7 +45,7 @@ int main()
     radix_sort(test_vec);
     
     cout << "{ ";
-    lp(i, test_vec.size()-1) {
+    lp(i, test_vec.size()) {
 		cout << test_vec[i] << " ";
 	}
 	cout << "}" << endl;
